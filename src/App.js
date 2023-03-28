d885fba801afcdb6eedf082f013abbae5b7406af
sagnnax <axel.gustavo2001@outlook.com>
2023-03-28 10:05:26 -0600

    update front-reat

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          sagnate
+          sagnax
         </p>
         <a
           className="App-link"
